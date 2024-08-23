a65a1d3e9bac135009cd213744936d6bdff6ad49
Breyten Cowen <breyten.cowen@gmail.com>
2024-08-22 21:24:38 -0400

    Started dashboard frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,28 @@ import "./App.css";
 import { Link } from "react-router-dom";
 import Landing from "./Landing";
 import landingScrolling from "./landingLogic";
-
+import { useEffect } from "react";
 function App() {
+  
+    useEffect(() => {
+      const anchorLinks = document.querySelectorAll('nav a[href^="#"]');
+      anchorLinks.forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+          e.preventDefault();
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+          });
+        });
+      });
+    }, []);
+  
   return (
     <>
       <button>
         <Link to="/Dashboard">Dashboard Link</Link>
       </button>
       <Landing />
-      <landingScrolling />
+    
     </>
   );
 }
