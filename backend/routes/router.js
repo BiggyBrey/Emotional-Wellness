23f1c76e76af3efdf8f9ae82aa30ebbdfcb5f09e
visioncode06 <thecoderdog06@gmail.com>
2024-09-05 17:19:48 -0400

    created aichat model + router, install axios in backend, still fixing some bugs

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { userRouter } from "./userRouter.js";
 import { journalRouter } from "./journalRouter.js";
-
+import { AiChatRouter } from "./aiConvoRouter.js";
 const router = express.Router();
 
 router.use("/users", userRouter);
 router.use("/journals", journalRouter);
+router.use("/aiChats", AiChatRouter)
 
 export default router;
---
+++ frontend/src/services/openAiApi.js
@@ -0,0 +1,3 @@
+import axios from "axios";
+
+export const startChat = () =>axios.post()
