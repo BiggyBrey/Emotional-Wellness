99eb72876145bd49938e05e640c82742aad8ec6c
Michael <micaheledquilan@gmail.com>
2024-09-12 19:10:34 -0400

    fixed bugs for aijournal

--- frontend/src/components/JournalHistory.jsx
+++ frontend/src/components/JournalHistory.jsx
@@ -1,15 +1,25 @@
+import { useState } from "react"
 import AiJournalModal from "./AiJournalModal"
 export default function JournalHistory(props) {
+    const [showModal, setShowModal] = useState(false)
     return (
         <>
-            <div>
-                {props.mood}
-                {props.content}
-                <AiJournalModal
-                    deleteChat={props.deleteChat}
-                    convoID={props.convoID}
-                    content={props.content}
-                />
+            <div
+                className="flex justify-between p-2 m-1"
+                onMouseLeave={() => { setShowModal(false) }}
+                onMouseEnter={() => { setShowModal(true) }}>
+                <div>
+                    {props.mood} {props.content}
+                </div>
+
+
+                {showModal &&
+                    <AiJournalModal
+                        deleteChat={props.deleteChat}
+                        convoID={props.convoID}
+                        content={props.content}
+                    />
+                }
             </div>
         </>
     )
